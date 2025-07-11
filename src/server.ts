7e4107269d9de6f397a8f9de036dbb4b2fb87092
Artur Moises <arturmoiscontato@gmail.com>
2025-07-11 13:35:33 -0300

    Update response message in server.ts to 'Hello, world V2!'

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello, world!' });
+  res.json({ message: 'Hello, world V2!' });
 });
 
 app.listen(port, () => {
